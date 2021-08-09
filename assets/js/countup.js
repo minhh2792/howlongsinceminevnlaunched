efac2e32421080ab7e2ca8b86a78bf3a8b5d7a78
Nguyễn N. Thiện <thiennnfx10909@funix.edu.vn>
2021-08-09 08:50:13 +0700

    updated

--- assets/js/countup.js
+++ assets/js/countup.js
@@ -1,8 +1,8 @@
 let countUpDate = new Date("Sep 4, 2015 16:00:00");
 let startedYear = countUpDate.getFullYear();
 
-    // Update every 1s
-    setInterval(function() {
+// Update every 1s
+setInterval(function() {
 
     let now = new Date().getTime();
 
@@ -29,10 +29,10 @@ let startedYear = countUpDate.getFullYear();
     let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
+
     let kiwi = "1";
     let accDay = days - kiwi;
 
     document.getElementById("result").innerHTML = accYear + " năm " + accMonth + " tháng " + accDay + " ngày " + hours + " giờ " + minutes + " phút và " + seconds + " giây";
-    
-    }, 1000)
+
+}, 1000)
