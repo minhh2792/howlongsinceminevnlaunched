c1c7982394122377f075e5ce1dc3137bcd908ae6
acayrin <38790060+acayrin@users.noreply.github.com>
2021-09-06 15:01:38 +0700

    igu

--- assets/js/countup.js
+++ assets/js/countup.js
@@ -1,15 +1,20 @@
 let from = new Date("Sep 6, 2015 19:00:00")
-function printStartedSince() {
-    let z  = new Date()
-    let x  = Math.floor(z.getTime() / 1000 - from.getTime() / 1000) // get difference in seconds
-    let vy = Math.floor( x / 31556926 )                             // get years
-    let vm = Math.floor( x % 31556926 / 2629743 )                   // get months
-    let vd = Math.floor( x % 31556926 % 2629743 / 86400 )           // get days
-    let vh = Math.floor( x % 31556926 % 2629743 % 86400 / 3600 )    // get hours
-    let vM = z.getMinutes()                                         // get minutes
-    let vs = z.getSeconds()                                         // get seconds
 
-    document.getElementById("result").innerHTML = `${vy} năm ${vm} tháng ${vd} ngày ${vh} giờ ${vM} phút và ${vs} giây`
+function printStartedSince() {
+    // bruh
+    var m1 = moment(from)
+    var m2 = moment()
+    var md = moment.duration(m2.diff(m1))
+    var m  = {
+      yy: md.years(),
+      mm: md.months(),
+      dd: md.days(),
+      HH: md.hours(),
+      MM: md.minutes(),
+      SS: md.seconds()
+    }
+
+    document.getElementById("result").innerHTML = `${m.yy} năm ${m.mm} tháng ${m.dd} ngày ${m.HH} giờ ${m.MM} phút và ${m.SS} giây`
 }
 
 // Update every 1s
